4fb8b2dc5bf3971351bcbe3ab69db1e9e68f08ce
aqsmalhotra <n01249822@humbermail.ca>
2018-06-27 19:20:11 -0400

    add car model and component

---
+++ src/app/cars/car.ts
@@ -0,0 +1,14 @@
+export class Car {
+    id: number;
+    type: string;
+    year: number;
+    make: string;
+    model: string;
+    transmission_type: string;
+    vehicle_status: string;
+    price: number;
+    fuel_type: string;
+    kilometres: number;
+    has_carproof: boolean;
+    date_posted: Date
+}
